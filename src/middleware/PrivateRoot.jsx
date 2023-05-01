31d877c80dc849f19dd1f0ed1cec5104a09c84c0
Dhimas Pandu Yogaswara <79622243+Dhimasswara@users.noreply.github.com>
2023-05-01 17:18:08 +0700

    edit middleware

--- src/middleware/PrivateRoot.jsx
+++ src/middleware/PrivateRoot.jsx
@@ -5,7 +5,7 @@ const PrivateRoute = ({children}) => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    if(localStorage.getItem('token', null) || localStorage.getItem('token') === null ) return navigate('/login')
+    if(localStorage.getItem('token', null)) return navigate('/login')
   }, [])
   return children
 }
